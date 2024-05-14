17e2f8392ce7d3fa0f988ca9ff9448afd467cd76
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-05-14 19:31:28 -0300

    commit

--- src/main/java/main/TpParqueDiversionesEda.java
+++ src/main/java/main/TpParqueDiversionesEda.java
@@ -1,8 +1,31 @@
 package main;
 
-public class TpParqueDiversionesEda {
+import tda.Fila;
 
+public class TpParqueDiversionesEda {
+    
+    
+    /*
+    Registro de entrada de visitantes: Cuando un visitante llega al parque, se
+    agrega a la cola general de entrada. Cada visitante tiene un identificador único
+    */
+    
+    public static void agregarVisitante(Fila visitante, int idVisitante, String nombre){
+        visitante.insertar(new Visitante(idVisitante, nombre));
+    }
+    
+    /*
+    Control de capacidad general: El sistema debe verificar periódicamente si el
+    número total de visitantes dentro del parque ha alcanzado la capacidad máxima
+    permitida. Si es así, el sistema debe detener temporalmente la entrada de nuevos
+    visitantes hasta que haya suficiente espacio disponible dentro del parque.
+    */
+    
+    public static void controlCapacidad(){
+        
+    }
+    
     public static void main(String[] args) {
-        Fila<Visitante> = new Fila();
+        Fila<Visitante> visitante = new Fila();
     }
 }
