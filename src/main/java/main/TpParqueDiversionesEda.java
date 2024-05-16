ad98ffecdaf3d1ae6d48d5acc667010fb6dc2959
Kyrre1356 <jorge1356@hotmail.com>
2024-05-15 22:49:45 -0300

    corregido despedir

--- src/main/java/main/TpParqueDiversionesEda.java
+++ src/main/java/main/TpParqueDiversionesEda.java
@@ -32,6 +32,7 @@ public class TpParqueDiversionesEda {
                 visitante.eliminar();
                 estado = true;
             }
+            visitante.insertar(visitante.eliminar());
         }
         visitantesActuales--;
         return estado;
